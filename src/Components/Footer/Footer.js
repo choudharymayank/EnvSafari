b226e3e77d90955c0c569d5e6c2aeefa5518727c
choudharymayank <choudharymayank6919@gmail.com>
2023-05-20 22:53:18 +0530

    Added Source Code Link

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 function Footer(){
 
     return <div className="footer">
-        <a href="www.google.com">Download Soure Code</a>
+        <a target="_blank" href="https://github.com/choudharymayank/EnvSafari">Download Soure Code</a>
             <p>Developed by Mayank Choudhary</p>
     </div>
 
